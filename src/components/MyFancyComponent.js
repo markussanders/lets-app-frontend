20a55c55648068959988857b75e50e55f0e9aad7
Markus Sanders <markus.sanderst@gmail.com>
2019-09-05 14:17:25 -0500

    Update MyFancyComponent.js

--- src/components/MyFancyComponent.js
+++ src/components/MyFancyComponent.js
@@ -8,7 +8,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCvCLXH3XB-vRnzD7xrKfqBW6Pp5uYcbLg&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=APIKEYHERE&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
